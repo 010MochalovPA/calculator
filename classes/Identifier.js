3525c1646bd49f571b4cef1d90cec2b50571a5fc
Павел Мочалов <010mochalovpa@gmail.com>
2022-06-05 19:55:52 +0300

    refactor: change variable name

--- classes/Identifier.js
+++ classes/Identifier.js
@@ -8,7 +8,7 @@ class Identifier {
   }
 
   isValid() {
-    if (this._invalidValue.includes(this.value)) return false;
+    if (this._invalidValues.includes(this.value)) return false;
     if (/^[a-zA-Z_]+([a-zA-Z0-9_]+)?$/.test(this.value)) return true;
     if (/^[-]?[0-9]+[.]?[0-9]*$/.test(this.value)) return true;
     return false;
