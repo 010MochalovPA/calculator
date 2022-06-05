0a18f699b49e21622045c3514bbf9b1679e2caa9
Павел Мочалов <010mochalovpa@gmail.com>
2022-06-05 09:36:27 +0300

    feat: add var function

--- classes/Data.js
+++ classes/Data.js
@@ -1,6 +1,18 @@
+import Variable from './Variable.js';
+import Function from './Function.js';
+
 class Data {
   constructor(){
+    this.variables = [];
+    this.functions = [];
+  }
 
+  createNumericVar(value){
+    this.variables.push(new Variable(value, NaN));
+    return;
+  }
+  createNumericVar(){
+    
   }
 }
 
--- modules/commands.js
+++ modules/commands.js
@@ -1,6 +1,33 @@
+import Data from '../classes/Data.js';
+import Identifier from '../classes/Identifier.js';
+import print from '../modules/print.js'
+
+const data = new Data;
+
 const commands = {
 
   var(value) {
+
+    let identifier = new Identifier(value);
+
+    if (identifier.isNumeric() || !identifier.isValid()) {
+      print.addOutput(`Введите "var <идентификатор>"!`);
+      print.addOutput(`Можно использовать буквы английского алфавита, цифры и символ подчеркивания. Идентификатор не может начинаться с цифры.`);
+      print.addOutput(`-----------------`);
+      return;
+    }
+
+    if(data.isUsed(identifier)){
+      print.addOutput(`"${identifier.getValue()}" уже существует!`);
+      print.addOutput(`-----------------`);
+      return;
+    }
+
+    if (identifier.isNumeric()) data.createNumericVar(identifier.getValue());
+    if (!identifier.isNumeric()) data.createValueVar(identifier.getValue());
+    data.addVar(identifier);
+    print.addInput(`var ${value}`);
+    
   },
 
   let(value) {
