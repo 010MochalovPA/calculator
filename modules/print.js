d0b4d77a173b2e8b5ad461a92497f1f51f2453e4
Павел Мочалов <010mochalovpa@gmail.com>
2022-06-05 09:10:45 +0300

    feat: add print module

---
+++ modules/print.js
@@ -0,0 +1,20 @@
+const inputField = document.querySelector('#input');
+const outputField = document.querySelector('#output');
+
+
+const print = {
+  addInput: (value) => {
+    inputField.value += `> ${value}\n`;
+  },
+  addOutput: (value) => {
+    outputField.value += `${value}\n`;
+  },
+  clearInput: () => {
+    inputField.value = ``;
+  },
+  clearOutput: ()=> {
+    outputField.value = ``;
+  }
+}
+
+export default print;
